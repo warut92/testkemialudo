61e0da4e7bf723e8333e93f4124fa5a556e894cb
wrt (: ) <warut92@gmail.com>
2024-01-04 16:58:25 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,52 +1,75 @@
-//ตั้งตัวแปร
-const cationArr = ["Na<sup>+</sup", "Ca<sup>+</sup"];
-const anionArr = ["Cl<sup>-</sup", "Br<sup>-</sup"];
+//ตั้งตัวแปร cation กับ anion
+const cationArr = ["Na<sup>+</sup>", "Ca<sup>+</sup>"];
+const anionArr = ["Cl<sup>-</sup>", "Br<sup>-</sup>"];
 
 //สร้างตัวแปรสุ่น
-let randomNum1 = ""
-let randomNum2 = ""
+let randomCationIndex = "";
+let randomAnionIndex = "";
 
 //ตัวแปรสำหรับคำตอบ
-var answerKemioArr = []
-let answerKemioStr =""
+var allIonicCompounds = [];
+let selecetedIonicCompounds = "";
 
 //ฟังก์ชันสุ่ม
 function random() {
   //ส่งลบข้อความตรวจคำตอบ
-  document.getElementById('reply').innerHTML = ""
+  document.getElementById('reply').innerHTML = "";
 
   //กำหนดคำตอบในรูปแบบของ 2D array
-  answerKemioArr = [
+  allIonicCompounds = [
     ["sodiumchloride", "sodiumbromide"],
     ["calciumchloride", "calciumbromide"],
-  ]
-  //สุ่ม
-  randomNum1 = Math.floor(Math.random() * 2);
-  randomNum2 = Math.floor(Math.random() * 2);
+  ];
+  //สุ่ม มีค่าเริ่มต้นจาก 0
+  randomCationIndex = Math.floor(Math.random() * cationArr.length);
+  randomAnionIndex = Math.floor(Math.random() * anionArr.length);
 
-//ส่งออกตัวไอออน
-  document.getElementById('cation').innerHTML = cationArr[randomNum1];
-  document.getElementById('anion').innerHTML = anionArr[randomNum2];
-  answerKemioStr = answerKemioArr[randomNum1][randomNum2]
+  //แสดง cation กับ anion ทางหน้าจอ
+  document.getElementById('cation').innerHTML = cationArr[randomCationIndex];
+  document.getElementById('anion').innerHTML = anionArr[randomAnionIndex];
+
+  //ดึงชื่อของสารประกอบจาก IonicCompounds
+  selecetedIonicCompounds = allIonicCompounds[randomCationIndex][randomAnionIndex];
 }
+//สั่ง random() ตั้งแต่ตอนโหลดเพจ
 random()
+
+//ตัวแปรสำหรับคะแนน
+let score = 0;
+//ในครั้งแรก กำหนดให้คะแนนเท่ากับ 0
+if (localStorage.userScore === undefined) {
+  localStorage.userScore = 0
+}
+//ดึงคะแนนจาก localStorage
+document.getElementById('score').innerHTML = localStorage.userScore;
+
 function checkAnswer() {
-  console.log('ANSWERKEMIOSTR', answerKemioStr)
+  //ดึงคำตอบของผู้ใช้
   let answerFromUser = document.getElementById('answer_user').value;
-  console.log('ANSWERFROMUSER ', answerFromUser )
+  //ถ้าผู้ใช้ไม่กรอกคำตอบ ให้แสดงผลว่าต้องกรอกคำตอบก่อน
   if (answerFromUser === "") {
-    document.getElementById('reply').innerHTML = "กรุณากรอกคำตอบ"
-  } else if (answerKemioStr === answerFromUser) {
-    document.getElementById('reply').innerHTML = "ถูกต้องครับ รอการสุ่มอัตโนมัติ"
-    setTimeout(deleteAns, 1500)
-    setTimeout(random, 1500)
+    document.getElementById('reply').innerHTML = "กรุณากรอกคำตอบ";
+    //หากคำตอบของผู้ใช้ตรงกับคำตอบที่ถูกต้อง
+  } else if (selecetedIonicCompounds === answerFromUser) {
+    document.getElementById('reply').innerHTML = "ถูกต้องครับ รอการสุ่มอัตโนมัติ";
+    //เพิ่มทีละ 1 คะแนน
+    score++
+    //แสดงค่าคะแนน
+    document.getElementById('score').innerHTML = score;
+    //ส่งคะแนนเข้า localStorage
+    score
+    localStorage.userScore = Number(localStorage.userScore) + 1;
+    //ลบข้อความตรวจคำตอบ
+    setTimeout(deleteAnsUser, 1500);
+    //
+    setTimeout(random, 1500);
   } else {
-    document.getElementById('reply').innerHTML = "ไม่ถูกครับ"
-    setTimeout(deleteAns, 1000)
+    document.getElementById('reply').innerHTML = "ไม่ถูกครับ";
+    setTimeout(deleteAnsUser, 1000);
   }
 }
 
 //ฟังก์ชันลบคำตอบที่ผู้ใช้ตอบมา
-function deleteAns() {
-  document.getElementById('answer_user').value = ""
+function deleteAnsUser() {
+  document.getElementById('answer_user').value = "";
 }
